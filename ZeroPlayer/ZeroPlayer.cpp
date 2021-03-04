2473e4aaf578e1204d1c449f18132128593cbf96
flfan <1490459938@qq.com>
2021-03-04 17:31:43 +0800

    init cmake project

---
+++ ZeroPlayer/ZeroPlayer.cpp
@@ -0,0 +1,12 @@
+﻿// ZeroPlayer.cpp : Defines the entry point for the application.
+//
+
+#include "ZeroPlayer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello ZeroPlayer." << endl;
+	return 0;
+}
